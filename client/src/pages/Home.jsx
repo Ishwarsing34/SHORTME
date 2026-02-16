c6cf08728720076ee5ed2ed848a1e1ae31a23e22
Ishwarsing34 <igirase65@gmail.com>
2026-02-16 22:03:34 +0530

    project done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
 
       {/* 🔥 GitHub Button */}
       <a
-        href="https://github.com/yourusername"
+        href="https://github.com/Ishwarsing34/SHORTME"
         target="_blank"
         rel="noopener noreferrer"
         className="absolute top-6 right-6 bg-black text-white px-4 py-2 rounded-full flex items-center gap-2 hover:scale-105 transition"
